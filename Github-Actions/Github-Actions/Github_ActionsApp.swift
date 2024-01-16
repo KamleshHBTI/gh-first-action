ac57ce94634d35c2244a7ec45e1523b29690673c
kamleshkumar <kamlesh.kumar@wibmo.com>
2024-01-16 16:49:13 +0530

    initial commit

---
+++ Github-Actions/Github-Actions/Github_ActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ActionsApp.swift
+//  Github-Actions
+//
+//  Created by Kamalesh Kumar on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_ActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
